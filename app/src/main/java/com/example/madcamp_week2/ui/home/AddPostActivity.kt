83a2c0590a4e7acb8a5e7a583117bc96912ebb0f
minizzang <79495204+minizzang@users.noreply.github.com>
2022-01-06 15:08:28 +0900

    Add : recyclerView in home fragment

---
+++ app/src/main/java/com/example/madcamp_week2/ui/home/AddPostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.madcamp_week2.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madcamp_week2.R
+
+class AddPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+    }
+}
--- app/src/main/java/com/example/madcamp_week2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/madcamp_week2/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.madcamp_week2.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +9,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madcamp_week2.R
 import com.example.madcamp_week2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    lateinit var homeItemAdapter: HomeItemAdapter
+    val items = mutableListOf<ItemData>()
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -19,16 +23,38 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        initRecycler()
+
+        val btn_add: View = binding.btnFloatingAdd
+        btn_add.setOnClickListener{ view ->
+            val intent = Intent(context, AddPostActivity::class.java)
+            startActivity(intent)
+        }
+
         return root
+    }
+
+    private fun initRecycler() {
+        homeItemAdapter = HomeItemAdapter(requireContext())
+        binding.mainRecycler.adapter = homeItemAdapter
+
+        items.apply {
+            add(ItemData("충전기", "1월 6일", 500))
+            add(ItemData("충전기", "1월 6일", 500))
+            add(ItemData("충전기", "1월 6일", 0))
+            add(ItemData("충전기", "1월 6일", 0))
+            add(ItemData("충전기", "1월 6일", 500))
+            add(ItemData("충전기", "1월 6일", 500))
+
+
+            homeItemAdapter.items = items
+            homeItemAdapter.notifyDataSetChanged()
+        }
     }
 
     override fun onDestroyView() {

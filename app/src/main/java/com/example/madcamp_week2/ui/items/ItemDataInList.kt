d4e2a3141d3b0d54306e767478d3d34760179d16
jjminsuh <minsuh3203@gmail.com>
2022-01-08 16:52:34 +0900

    added item history UI

---
+++ app/src/main/java/com/example/madcamp_week2/ui/items/ItemDataInList.kt
@@ -0,0 +1,7 @@
+package com.example.madcamp_week2.ui.items
+
+data class ItemDataInList(
+    val itemName: String,
+    val nickName: String,
+    val lendPeriod: String
+)
---
+++ app/src/main/java/com/example/madcamp_week2/ui/items/LendItemAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.madcamp_week2.ui.items
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madcamp_week2.R
+
+class LendItemAdapter (val context: Context, val itemArrayInList: ArrayList<ItemDataInList>) : RecyclerView.Adapter<LendItemAdapter.LendItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : LendItemAdapter.LendItemViewHolder{
+        return LendItemViewHolder(LayoutInflater.from(context).inflate(R.layout.history_item, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return itemArrayInList.size
+    }
+
+    override fun onBindViewHolder(holder: LendItemViewHolder, position: Int) {
+        val item = itemArrayInList[position]
+        holder.bindLendItem(item)
+    }
+
+    inner class LendItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        var hItemName = itemView.findViewById<TextView>(R.id.itemName)
+        var hNickName = itemView.findViewById<TextView>(R.id.personName)
+        var hLendPeriod = itemView.findViewById<TextView>(R.id.Time)
+
+        fun bindLendItem(itemInList: ItemDataInList){
+            hItemName.text = itemInList.itemName
+            hNickName.text = itemInList.nickName
+            hLendPeriod.text = itemInList.lendPeriod
+
+            itemView.setOnClickListener{
+
+            }
+        }
+
+    }
+
+}

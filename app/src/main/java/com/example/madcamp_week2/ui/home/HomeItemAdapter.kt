95c38970ca1b01229a694205da44d33a5d41b2d3
minizzang <79495204+minizzang@users.noreply.github.com>
2022-01-09 22:27:33 +0900

    Fix : home recycler view item

--- app/src/main/java/com/example/madcamp_week2/ui/home/HomeItemAdapter.kt
+++ app/src/main/java/com/example/madcamp_week2/ui/home/HomeItemAdapter.kt
@@ -9,12 +9,22 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madcamp_week2.R
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.madcamp_week2.BASE_URL
+import org.json.JSONArray
+import org.json.JSONObject
 import kotlin.coroutines.coroutineContext
 
 
 class HomeItemAdapter(private val context: Context) : RecyclerView.Adapter<HomeItemAdapter.ViewHolder>(){
 
     var items = ArrayList<ItemData>()
+    private val baseURL = BASE_URL
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeItemAdapter.ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.home_item_recycler, parent, false)
@@ -29,19 +39,47 @@ class HomeItemAdapter(private val context: Context) : RecyclerView.Adapter<HomeI
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val itemName: TextView = itemView.findViewById(R.id.tv_itemName)
-        private val itemDate: TextView = itemView.findViewById(R.id.tv_itemDate)
-        private val itemPrice: TextView = itemView.findViewById(R.id.tv_itemPrice)
+        private val itemDateStart: TextView = itemView.findViewById(R.id.tv_itemDateStartShow)
+        private val itemDateEnd: TextView = itemView.findViewById(R.id.tv_itemDateEndShow)
+        private val itemPrice: TextView = itemView.findViewById(R.id.tv_itemPriceShow)
 
         fun bind(item : ItemData) {
             itemName.text = item.name
-//            itemDate.text = item.date
-            itemPrice.text = item.price.toString()
+            itemDateStart.text = item.date_start+" ~ "
+            itemDateEnd.text = item.date_end
+            itemPrice.text = item.price.toString()+"원"
 
             itemView.setOnClickListener{
-                val intent = Intent(context, ItemDetailActivity::class.java)
-                context.startActivity(intent)
+
+                //item_id를 넣으면 어떤 유저의 item인지 리턴
+                serverGetItemOwner(item.item_id)
             }
 
         }
+    }
+
+    fun serverGetItemOwner(item_id :String) {
+        val requestQueue = Volley.newRequestQueue(context)
+        val stringRequest = object : StringRequest(
+            Request.Method.GET, "$baseURL"+"/api/getItemOwner/${item_id}",
+            Response.Listener<String> { res ->
+                val item_owner_id = JSONObject(res).getString("user_id")
+
+                //shared preference에서 user_id 가져오기
+//                val to_user_id =
+
+                val intent = Intent(context, ItemDetailActivity::class.java)
+                intent.putExtra("item_id", item_id)
+                intent.putExtra("from_user", item_owner_id)
+//                intent.putExtra("to_user", to_user_id)
+
+                context.startActivity(intent)
+
+            },
+            Response.ErrorListener { err ->
+                Log.d("getItemOwner", "error! $err")
+            }){}
+
+        requestQueue.add(stringRequest)
     }
 }
--- app/src/main/java/com/example/madcamp_week2/ui/home/ItemDetailActivity.kt
+++ app/src/main/java/com/example/madcamp_week2/ui/home/ItemDetailActivity.kt
@@ -3,10 +3,16 @@ package com.example.madcamp_week2.ui.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.madcamp_week2.R
 import com.example.madcamp_week2.ui.chat.ChattingActivity
+import org.json.JSONObject
 
 class ItemDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +26,43 @@ class ItemDetailActivity : AppCompatActivity() {
             val intent = Intent(this, ChattingActivity::class.java)
             this.startActivity(intent)
         }
+
+        val borrowItem = findViewById<Button>(R.id.btn_borrow)
+
+        borrowItem.setOnClickListener {
+//            serverBorrowItem(from_user, to_user, contract_item)
+        }
     }
 
+//    fun serverBorrowItem(nickname:String) {
+//        val requestQueue = Volley.newRequestQueue(this)
+//        val stringRequest = object : StringRequest(
+//            Request.Method.POST, "$baseURL"+"/api/checkNickname",
+//            Response.Listener<String> { res ->
+//                val msg = JSONObject(res).getString("msg")
+//                if (msg == "duplicated") {
+//                    Log.d("checkNickname", "res : $msg")
+//                    handleNickname(true)
+//                } else {
+//                    handleNickname(false)
+//                }
+//            },
+//            Response.ErrorListener { err ->
+//                Log.d("checkNickname", "error! $err")
+//            }) {
+//            override fun getBodyContentType(): String {
+//                return "application/json"
+//            }
+//            override fun getBody(): ByteArray {
+//                val param = HashMap<String, String>()
+//                param.put("nickname", nickname)
+//                return JSONObject(param as Map<*, *>).toString().toByteArray()
+//            }
+//        }
+//        requestQueue.add(stringRequest)
+//    }
+
+    // action bar의 back button 구현
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item?.itemId) {
             android.R.id.home -> {

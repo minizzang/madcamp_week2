42dab339e9949a43600bcb5e2ac559454b2b3616
minizzang <79495204+minizzang@users.noreply.github.com>
2022-01-10 14:04:14 +0900

    Backend : load item datas into detail page

--- app/src/main/java/com/example/madcamp_week2/ui/home/ItemDetailActivity.kt
+++ app/src/main/java/com/example/madcamp_week2/ui/home/ItemDetailActivity.kt
@@ -3,16 +3,21 @@ package com.example.madcamp_week2.ui.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.madcamp_week2.BASE_URL
 import com.example.madcamp_week2.R
+import com.example.madcamp_week2.User
 import com.example.madcamp_week2.ui.chat.ChattingActivity
+import com.google.gson.Gson
+import org.json.JSONArray
 import org.json.JSONObject
 
 class ItemDetailActivity : AppCompatActivity() {
@@ -26,8 +31,6 @@ class ItemDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_item_detail)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        from_user = intent.getStringExtra("from_user").toString()
-        to_user = intent.getStringExtra("to_user").toString()
         item_id = intent.getStringExtra("item_id").toString()
 
         val startChatting = findViewById<Button>(R.id.startChatting)
@@ -37,11 +40,55 @@ class ItemDetailActivity : AppCompatActivity() {
             this.startActivity(intent)
         }
 
+        serverGetItemDetail(item_id)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
         val borrowItem = findViewById<Button>(R.id.btn_borrow)
 
         borrowItem.setOnClickListener {
             serverBorrowItem(from_user, to_user, item_id)
         }
+    }
+
+    fun serverGetItemDetail(item_id :String) {
+        val requestQueue = Volley.newRequestQueue(this)
+        val stringRequest = object : StringRequest(
+            Request.Method.GET, "$baseURL"+"/api/getItemDetail/${item_id}",
+            Response.Listener<String> { res ->
+                val resObj = JSONArray(res)[0].toString()
+                val item_name = JSONObject(resObj).getString("item_name")
+                val item_place = JSONObject(resObj).getString("item_place")
+                val item_price = JSONObject(resObj).getString("item_price")
+                val item_date_start = JSONObject(resObj).getString("item_date_start")
+                val item_date_end = JSONObject(resObj).getString("item_date_end")
+                val item_description = JSONObject(resObj).getString("item_description")
+
+                findViewById<TextView>(R.id.ItemNameDetail).text = item_name
+                findViewById<TextView>(R.id.ItemPlaceDetail).text = item_place
+                findViewById<TextView>(R.id.ItemPriceDetail).text = item_price
+                findViewById<TextView>(R.id.ItemDateStartDetail).text = item_date_start
+                findViewById<TextView>(R.id.ItemDateEndDetail).text = item_date_end
+                findViewById<TextView>(R.id.ItemDescriptionDetail).text = item_description
+
+                //shared preference에서 user_id 가져오기
+                val appSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+                val gson = Gson()
+                var json = appSharedPreferences.getString("user", "")
+                var obj = gson.fromJson(json, User::class.java)
+
+                from_user = JSONObject(resObj).getString("user_id")
+                to_user = obj.id.toString()
+            },
+            Response.ErrorListener { err ->
+                Log.d("getItemDetail", "error! $err")
+            }){}
+
+        requestQueue.add(stringRequest)
     }
 
     fun serverBorrowItem(from_user:String, to_user:String, item_id:String) {
